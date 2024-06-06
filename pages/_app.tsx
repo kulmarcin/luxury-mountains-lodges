eb5493a789300f72c6503b9fea1eb1b84283d2c0
Marcin <kul.marcin@gmail.com>
2024-06-06 19:50:11 +0200

    added og meta and icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import '../styles/datePicker.css';
 import '../styles/loader.css';
 import GlobalStyle from '../styles/globalStyle';
 import Loader from '../components/Loader';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [loading, setLoading] = useState(true);
@@ -15,6 +16,35 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
+      <Head>
+        <meta property="og:title" content="Luxury Mountain Lodges"></meta>
+        <meta
+          property="og:description"
+          content="Book a Luxury Mountain Lodge. Choose from dozens beautiful places."
+        />
+        <meta property="og:url" content="https://luxurylodges.com" />
+        <meta property="og:site_name" content="Luxury Mountain Lodges" />
+        <meta property="og:locale" content="en_US" />
+        <meta
+          property="og:image"
+          content="https://luxury-mountains-lodges.vercel.app/icon.png"
+        />
+        <meta property="og:image:width" content="200" />
+        <meta property="og:image:height" content="200" />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Luxury Mountain Lodges" />
+        <meta
+          name="twitter:description"
+          content="Book a Luxury Mountain Lodge. Choose from dozens beautiful places."
+        />
+        <meta
+          name="twitter:image"
+          content="https://luxury-mountains-lodges.vercel.app/icon.png"
+        />
+        <meta name="twitter:image:width" content="200" />
+        <meta name="twitter:image:height" content="200" />
+      </Head>
       {!loading ? (
         <>
           <GlobalStyle />
